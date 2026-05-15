6682a574819f2e4bbd9b533d04d46d549cc3e9c1
Nico Hoffmann <me@nhoffmann.com>
2026-05-15 22:19:27 +0200

    refact: New `Cms\Inventory` class

--- src/Cms/Site.php
+++ src/Cms/Site.php
@@ -253,7 +253,7 @@ class Site extends ModelWithContent
 
 		$kirby = $this->kirby();
 
-		return $this->inventory = Dir::inventory(
+		return $this->inventory = Inventory::for(
 			$this->root(),
 			$kirby->contentExtension(),
 			$kirby->contentIgnore(),
